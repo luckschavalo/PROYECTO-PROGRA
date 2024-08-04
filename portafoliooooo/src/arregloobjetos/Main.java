edec41ed2592ca1e94af8e29a2a9b7971be816f8
luckschavalo <lucaschl1007@gmail.com>
2024-08-03 23:37:06 -0600

    Delete portafoliooooo directory

--- portafoliooooo/src/arregloobjetos/Main.java
+++
@@ -1,9 +0,0 @@
-package arregloobjetos;
-public class Main {
-    public static void main(String[] args) {
-       Menu m=new Menu();
-       m.mostrarMenu();
-    }    
-}
-//se llama al menu 
-//holaaaaaaaaa 
